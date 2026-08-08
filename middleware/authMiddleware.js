a82dbae28f4687d2dfcbe67ea3d75c93e2900045
rahulkumar-hash <rahul.kumar@wishgeekstechserve.com>
2026-08-09 00:02:59 +0530

    update

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,11 @@
 module.exports = (req, res, next) => {
   if (!req.session.admin) {
-    return res.redirect("/login");
+    return res.redirect(
+      req.originalUrl.startsWith("/admin")
+        ? "/admin/login"
+        : "/login"
+    );
   }
+
   next();
 };
